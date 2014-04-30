50efe9230a6ac2a39197b0fd9acaf591aad6ca83
saftaclaudia <saftaclaudia27@yahoo.com>
2014-04-30 21:05:26 +0300

    products page
    php and css files

--- faq.php
+++ faq.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php $title="contacts";
+<?php $title="faq";
 	require('includes/head.php');
 ?>
 </head>
---
+++ products.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php $title="products";
+	require('includes/head.php');
+?>
+</head>
+<body>
+	<?php require ('includes/header.php'); ?>
+	<div class="topbar"></div>
+		<div class="container">
+			<div class="wrapper clearfix">
+				<div class="topcontainer clearfix">
+					<h2>Freatured Products</h2>
+					<div class="content width50  rightborder clearfix">
+						<img src="images/products1.png"/>
+						<span>Imperdiet doming quod mazim placerat facer possim</span>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elitsed diam nonummy nibh euismod tincidunt laoreet dolore magna aliquam erat volutpat.</p>
+					</div>
+					<div class=" content width50 leftborder clearfix">
+						<img src="images/products2.png">
+						<span>Lorem ipsum dolor sit amet, consectetuer</span>
+						<p>Adipiscing elitsed nonummy nibh euismod tincidunt laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud.<p>
+					</div>
+				</div>
+				<div class="bottomcontainer">
+					<div class="width60 clearfix">
+							<h2>Most Popular</h2>
+						<div class=" content product1 clearfix">
+							<img src="images/products3.png"/>
+							<span>Lorem ipsum dolor</span>
+							<p>Sit amet, consectetuer adipiscing elitsed diam nonummy nibh.</p>
+						</div>
+						<div class="content product2 clearfix">
+							<img src="images/products4.png"/>
+							<span>Lorem ipsum dolor</span>
+							<p>Sit amet, consectetuer adipiscing elitsed diam nonummy nibh.</p>
+						</div>
+						<div class="content product3 clearfix">
+							<img src="images/products5.png"/>
+							<span>Lorem ipsum dolor</span>
+							<p>Sit amet, consectetuer adipiscing elitsed diam nonummy nibh.</p>
+						    <a href="">View All</a>
+						</div>
+						<div class="content product4 clearfix">
+							<img src="images/products6.png"/>
+							<span>Lorem ipsum dolor</span>
+							<p>Sit amet, consectetuer adipiscing elitsed diam nonummy nibh.</p>
+						</div>
+					</div>
+					<div class="content width40">
+						<h2>Poducts List</h2>
+						<ul class="arow">
+							<li>At vero eos et accusam et justo duo </li>
+							<li>Dolores et ea restet clita kasd gubergren</li>
+							<li>Lorem ipsum dolor sit amet</li>
+							<li><p>Dorem ipsum dolor sit amet onsetetur</p></li>
+							<li><p>Sadipscing elitr, sed diam nonumy </p></li>
+							<li><p>Eirmod tempor invidunt ut labore</p></li>
+							<li><p>Dolore magna aliquyam erat, sed diam</p></li>
+						</ul>
+						<a href="">Read More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+				
+	<?php require('includes/footer.php'); ?>
+</body>
+</html>
+
+
+
